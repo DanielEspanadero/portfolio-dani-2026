2c474e0a2ad04ec17f9bf97ae3d2e199998dbdb3
Marcos <ma.salazar@redlink.com.ar>
2026-05-03 22:03:41 -0300

    se hicieron algunos arreglos en la parte del libro y de las tech

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,4 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   integrations: [sitemap()],
-  vite: {
-    server: {
-      allowedHosts: ['w7kbtl-ip-186-18-87-181.tunnelmole.net'],
-    },
-  },
 });
